acedd558e0e327d45ad0fa4c1843780c6454396c
Jonas Braga <jonasbraga2001@gmail.com>
2020-08-07 01:59:47 -0300

    feat: :sparkles: Added connection of front and back-end
    Using apis as a service to fill of the front-end information

--- aulas/web/src/components/TeacherItem/index.tsx
+++ aulas/web/src/components/TeacherItem/index.tsx
@@ -1,38 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css'
+import "./styles.css";
+import api from "../../services/api";
 
-function TeacherItem(){
-    return (
-        <article className="teacher-item">
-            <header>
-                <img src="https://via.placeholder.com/500x500.jpeg?text=Jonas%20Braga" alt="Jonas Braga"/>
-                <div>
-                    <strong>Jonas Braga</strong>
-                    <span>Programação</span>
-                </div>
-            </header>
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                <br/>
-                <br/>
-                Labore recusandae ab rerum sapiente voluptatum, repellat placeat ea odio, voluptas esse nobis, temporibus suscipit voluptatem quos. Quam vero tempore suscipit odio!
-            </p>
-            <footer>
-                <p>
-                    Preço/hora
-                    <strong>R$ 100,00</strong>
-                </p>
-                <button type="button">
-                    <img src={whatsappIcon} alt="Whatsapp"/>
-                    Entrar em contato
-                </button>
-            </footer>
-        </article>
-    );
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
 }
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          target="_blank"
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  );
+};
 
 export default TeacherItem;
