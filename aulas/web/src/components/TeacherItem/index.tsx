db0cbdcc7aee067eb34c4b6047bac8ebd4f804df
Jonas Braga <jonasbraga2001@gmail.com>
2020-08-11 00:23:39 -0300

    Initial: :tada: Initial commit for mobile aplication with expo

--- aulas/web/src/components/TeacherItem/index.tsx
+++ aulas/web/src/components/TeacherItem/index.tsx
@@ -45,6 +45,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           onClick={createNewConnection}
           href={`https://wa.me/${teacher.whatsapp}`}
           target="_blank"
+          rel="noopener noreferrer"
         >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
