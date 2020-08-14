21c77737d612d7f27180f1cda61acd944ef61824
Jonas Braga <jonasbraga2001@gmail.com>
2020-08-14 01:20:36 -0300

    feat: :construction: Mobile with list of teachers and some api integration

--- aulas/mobile/src/pages/Landing/index.tsx
+++ aulas/mobile/src/pages/Landing/index.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+
+import api from "../../services/api";
+
+import styles from "./styles";
 
 import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 
-import styles from "./styles";
-
 function Landing() {
   const { navigate } = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    });
+  }, []);
 
   function handleNavigationToGiveClassesPage() {
     navigate("GiveClasses");
@@ -46,7 +57,8 @@ function Landing() {
         </RectButton>
       </View>
       <Text style={styles.totalConections}>
-        Total de 200 conexões já realizadas <Image source={heartIcon} />
+        Total de {totalConnections} conexões já realizadas{" "}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
--- aulas/mobile/src/pages/TeacherList/index.tsx
+++ aulas/mobile/src/pages/TeacherList/index.tsx
@@ -1,13 +1,100 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, Text, TextInput } from "react-native";
+import { BorderlessButton, RectButton } from "react-native-gesture-handler";
 
 import PageHeader from "../../components/PageHeader";
 import styles from "./styles";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+
+import { Feather } from "@expo/vector-icons";
+import api from "../../services/api";
 
 function TeacherList() {
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeek_day] = useState("");
+  const [time, setTime] = useState("");
+
+  function handleToggleFiltersVisible() {
+    setIsFilterVisible(!isFilterVisible);
+  }
+
+  async function handleFiltersSubmit() {
+    const response = await api.get("classes", {
+      params: {
+        subject,
+        week_day,
+        time,
+      },
+    });
+    setIsFilterVisible(false);
+    setTeachers(response.data);
+  }
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#fff"></Feather>
+          </BorderlessButton>
+        }
+      >
+        {isFilterVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              value={subject}
+              onChangeText={(text) => setSubject(text)}
+              placeholder="Qual a matéria"
+              placeholderTextColor="#c1bccc"
+            ></TextInput>
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  style={styles.input}
+                  value={week_day}
+                  onChangeText={(text) => setWeek_day(text)}
+                  placeholder="Qual o dia?"
+                  placeholderTextColor="#c1bccc"
+                ></TextInput>
+              </View>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  value={time}
+                  onChangeText={(text) => setTime(text)}
+                  placeholder="Qual o horário?"
+                  placeholderTextColor="#c1bccc"
+                ></TextInput>
+              </View>
+            </View>
+            <RectButton
+              onPress={handleFiltersSubmit}
+              style={styles.submitButton}
+            >
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />;
+        })}
+      </ScrollView>
     </View>
   );
 }
